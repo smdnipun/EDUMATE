e1743dcfd1fa71838c1695985c97ca9abfd354bc
pamudithakakulandara <pamuditha@globalwavenet.com>
2022-09-03 19:28:34 +0530

    navbar and footer

---
+++ frontend/src/components/common.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Footer } from './footer'
+import Navbar from './navigationBar'
+
+export const Common = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Footer />
+    </div>
+  )
+}
