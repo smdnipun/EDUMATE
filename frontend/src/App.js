f6e55500c2cbed30f990436b0ce2081702ac0021
pamudithakakulandara <pamuditha@globalwavenet.com>
2022-09-04 10:27:38 +0530

    upload note interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Common } from './components/common';
+import { UploadNote } from './components/teacher/uploadNote';
 
 function App() {
   return (
     <div className="App">
-     <Common/>
+    <UploadNote/>
     </div>
   );
 }
