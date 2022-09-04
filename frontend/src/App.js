20cb14d4e30ab0ff22ccd15a82f0afeada5d273c
Hiroshac <hiroshachandramal01@gmail.com>
2022-09-04 11:26:17 +0530

    Update Upload note interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {BrowserRouter,Route,Routes} from 'react-router-dom';
 import { Navbar } from './components/common/Navbar';
 import { Footer } from './components/common/footer';
+import { UploadNote } from './components/client/teacher/UploadNote.js';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <UploadNote/>
      <BrowserRouter>
         <Routes>
             {/* admin */}
@@ -15,7 +17,7 @@ function App() {
 
         </Routes>
      </BrowserRouter>
-     <Footer/>
+     {/* <Footer/> */}
     </div>
   );
 }
--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Navbar = () => {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="index.html">EDUMATE</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
