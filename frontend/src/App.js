25821bcd5a16fa3cd3b3a6f01164f2e4c189b828
Hiroshac <hiroshachandramal01@gmail.com>
2022-09-05 19:53:57 +0530

    create student parts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,24 @@ import {BrowserRouter,Route,Routes} from 'react-router-dom';
 import { Navbar } from './components/common/Navbar';
 import { Footer } from './components/common/footer';
 import { UploadNote } from './components/client/teacher/UploadNote.js';
+import { StudentAnswersUpload } from './components/client/student/StudentAnswersUpload';
+import Home from './components/client/Home';
+import { ExamTimeTable } from './components/client/student/examtimetable/ExamTimeTable';
+import { FeedBack } from './components/client/student/feedback/FeedBack';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
-     <UploadNote/>
+     {/* <Home/> */}
      <BrowserRouter>
         <Routes>
             {/* admin */}
 
             {/* client */}
+            <Route exact path={'/studentanswersheetUpload'} element={<StudentAnswersUpload/>}/>
+            <Route exact path ={'/examtimetable'} element={<ExamTimeTable/>}/>
+            <Route exact path={'/feedback'} element={<FeedBack/>}/>
 
         </Routes>
      </BrowserRouter>
