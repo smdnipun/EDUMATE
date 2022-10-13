17f83dda9f12c588f6562fdd8d4e2dced7273cdc
Hiroshac <hiroshachandramal01@gmail.com>
2022-10-13 06:31:31 +0530

    student

--- frontend/src/components/common/Navigation/Navigation.js
+++ frontend/src/components/common/Navigation/Navigation.js
@@ -54,7 +54,7 @@ function Navigation() {
             <Navbar.Toggle aria-controls='responsive-navbar-nav' />
             <Navbar.Collapse id='responsive-navbar-nav'>
               <Nav className='me-auto'>
-                <Nav.Link href='#features'>Home</Nav.Link>
+                <Nav.Link href='/subjectnote'>Subject</Nav.Link>
                 <Nav.Link href='#pricing'>Pricing</Nav.Link>
                 <NavDropdown title='Dropdown' id='collasible-nav-dropdown'>
                   <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
